0390703b125c7e2b4a350c262c9a9afd4777fd82
TrayanDKirov <trayandimchev@gmail.com>
2025-07-04 15:20:18 +0300

    Added spring intializr for backend code

---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/TrippackerApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippackerApplication.class, args);
+	}
+
+}
---
+++ TripPacker-Source-Code/trippacker/src/test/java/bg/sofia/uni/fmi/tdkirov/trippacker/TrippackerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
