1b3c038d8e91f6ef313d783e56d5d4fccd4c3769
TrayanDKirov <trayandimchev@gmail.com>
2025-07-13 01:12:04 +0300

    Added TripLuggageService interface

---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/service/TripLuggageService.java
@@ -0,0 +1,58 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker.service;
+
+import bg.sofia.uni.fmi.tdkirov.trippacker.dto.TripLuggageCreateDto;
+import bg.sofia.uni.fmi.tdkirov.trippacker.dto.TripLuggageResponseDto;
+import bg.sofia.uni.fmi.tdkirov.trippacker.dto.TripLuggageUpdateDto;
+
+import java.util.Set;
+
+public interface TripLuggageService {
+    /**
+     * Creates a new TripLuggage entity and save it into the database using
+     * a creation data transfer object. And returns it's id.
+     *
+     * @param tripLuggageDto the creation data transfer object, must not be null.
+     * @param currentUser the username of the current user, must not be null.
+     *
+     * @return the id of the trip luggage in the database.
+     */
+    Long createTripLuggage(TripLuggageCreateDto tripLuggageDto, String currentUser);
+
+    /**
+     * Retrieves the TripLuggage entity associated with this id and returns it
+     * as a response data transfer object.
+     *
+     * @param id a unique identifier of the trip luggage, must not be null.
+     * @param currentUser the username of the current user, must not be null.
+     *
+     * @return the response data transfer object associated with this id.
+     */
+    TripLuggageResponseDto getTripLuggageById(Long id, String currentUser);
+
+    /**
+     * Retrieves all TripLuggage entities of the current user and returns them as
+     * an unmodifiable set of response data transfer objects.
+     *
+     * @param currentUser the username of the current user, must not be null.
+     *
+     * @return an unmodifiable set of response data transfer objects.
+     */
+    Set<TripLuggageResponseDto> getAllTripLuggage(String currentUser);
+
+    /**
+     * Updates an existing TripLuggage entity associated with this id and saves it into the database,
+     * using an update data transfer object.
+     *
+     * @param id a unique identifier of the trip luggage, must not be null.
+     * @param tripLuggageDto the update data transfer object, must not be null.
+     * @param currentUser the username of the current user, must not be null.
+     */
+    void updateTripLuggage(Long id, TripLuggageUpdateDto tripLuggageDto, String currentUser);
+
+    /**
+     * Deletes TripLuggage entity associated with this id from the database.
+     *
+     * @param id a unique identifier of the trip luggage, must not be null.
+     */
+    void deleteTripLuggageById(Long id, String currentUser);
+}
