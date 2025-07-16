d8fa6cc24285e0843cf471ddf7369bacb26eefee
TrayanDKirov <trayandimchev@gmail.com>
2025-07-16 14:39:04 +0300

    Added User entity

--- TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/User.java
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/User.java
@@ -1,4 +1,44 @@
 package bg.sofia.uni.fmi.tdkirov.trippacker.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE) // TODO Add a method changePassword
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+        this.createdAt = LocalDateTime.now();
+    }
 }
---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker.repository;
+
+import bg.sofia.uni.fmi.tdkirov.trippacker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
