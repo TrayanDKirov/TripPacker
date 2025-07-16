bbd0bae7c16d2fac63f9193f38a6692228aae5b8
TrayanDKirov <trayandimchev@gmail.com>
2025-07-16 14:23:24 +0300

    Defined PackingGroup entity

--- TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/PackingGroup.java
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/PackingGroup.java
@@ -1,5 +1,7 @@
 package bg.sofia.uni.fmi.tdkirov.trippacker.model;
 
+import bg.sofia.uni.fmi.tdkirov.trippacker.dto.packinggroup.PackingGroupCreateDto;
+import bg.sofia.uni.fmi.tdkirov.trippacker.dto.packinggroup.PackingGroupResponseDto;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,19 +9,57 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "packing_groups")
 public class PackingGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "group_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Setter(AccessLevel.NONE)
     private Set<Item> items;
+
+    @Column(name = "created_at", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", nullable = false)
+    private User creator;
+
+    public PackingGroup(String name, Set<Item> items) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+
+        if (items == null) {
+            this.items = new LinkedHashSet<>();
+        }
+    }
+
+    public static PackingGroup fromDtoAndUser(PackingGroupCreateDto packingGroupDto, User creator) {
+        return null;
+    }
+
+    public PackingGroupResponseDto toResponseDto() {
+        return null;
+    }
 }
---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/User.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker.model;
+
+public class User {
+}
