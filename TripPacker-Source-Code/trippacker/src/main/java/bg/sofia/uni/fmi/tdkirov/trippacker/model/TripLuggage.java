5c6090aaa74cd07a9f7859246b76828a7e937a13
TrayanDKirov <trayandimchev@gmail.com>
2025-08-02 18:12:36 +0300

    Finished TripLuggage entity

--- TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/TripLuggage.java
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/TripLuggage.java
@@ -1,13 +1,24 @@
 package bg.sofia.uni.fmi.tdkirov.trippacker.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -19,7 +30,44 @@ public class TripLuggage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "trip_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Setter(AccessLevel.NONE)
+    private Set<PackingGroup> packingGroups;
+
+    @OneToMany(mappedBy = "trip_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Setter(AccessLevel.NONE)
+    private Set<PackingItem> packingItems;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", nullable = false)
+    private User creator;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
     public TripLuggage(Long id) {
         this.id = id;
+    }
+
+    public TripLuggage(String name, User creator) {
+        this.name = name;
+        this.creator = creator;
+
+        this.packingGroups = new LinkedHashSet<>();
+        this.packingItems = new LinkedHashSet<>();
+
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    public void addPackingGroup(PackingGroup packingGroupToAdd) {
+        this.packingGroups.add(packingGroupToAdd);
+    }
+
+    public void addPackingItem(PackingItem packingItemToAdd) {
+        this.packingItems.add(packingItemToAdd);
     }
 }
