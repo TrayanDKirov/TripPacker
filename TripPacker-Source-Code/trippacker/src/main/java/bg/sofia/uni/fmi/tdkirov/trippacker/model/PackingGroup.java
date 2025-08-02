dbfa3a092ae7e595e17cc862657485b43e838b4e
TrayanDKirov <trayandimchev@gmail.com>
2025-08-02 12:03:13 +0300

    Added PackingItem and PackingGroup

---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/PackingGroup.java
@@ -0,0 +1,52 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "packing_groups")
+public class PackingGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private GroupToPack group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trip_id", nullable = false)
+    private TripLuggage trip;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", nullable = false)
+    private User creator;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public PackingGroup(GroupToPack group, TripLuggage trip, User creator) {
+        this.group = group;
+        this.trip = trip;
+        this.creator = creator;
+
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ TripPacker-Source-Code/trippacker/src/main/java/bg/sofia/uni/fmi/tdkirov/trippacker/model/PackingItem.java
@@ -0,0 +1,65 @@
+package bg.sofia.uni.fmi.tdkirov.trippacker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "packing_items")
+public class PackingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "packed_quantity", nullable = false)
+    private Integer packedQuantity;
+    @Column(name = "is_packed", nullable = false)
+    private Boolean isPacked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private ItemToPack item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trip_id", nullable = false)
+    private TripLuggage trip;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private PackingGroup group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", nullable = false)
+    private User creator;
+
+    @Column(name = "created_at", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    public PackingItem(Integer packedQuantity, ItemToPack itemToPack,
+                       TripLuggage trip, PackingGroup group, User creator) {
+        this.packedQuantity = packedQuantity;
+        this.isPacked = false;
+        this.trip = trip;
+        this.group = group;
+        this.creator = creator;
+
+        this.createdAt = LocalDateTime.now();
+    }
+}
